18ca93e3869ce709fbbeb44d4fb476758766f3f1
Laiba Sajid <sajid.laiba378@gmail.com>
2021-10-14 12:47:30 -0400

    base tests passing

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello')
+let url = "https://ghibliapi.herokuapp.com/films"
+
+fetch(url)
+    .then((res)=>{
+        return res.json();
+    }).then((movies)=>{
+        console.log(movies);
+        for(let movie of movies){
+            console.log(movie.title)
+        let select = document.querySelector("#select-movie");
+        ///create options
+        let newOption = document.createElement("option");
+        newOption.textContent = movie.title;
+        newOption.value = movie.title
+        select.append(newOption)
+        }
+    }).catch((err)=>{
+        console.log(err)
+    })
