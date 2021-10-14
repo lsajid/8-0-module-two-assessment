e233c7e01b9514704cf4ed73c5fe780dd03f51b0
Laiba Sajid <sajid.laiba378@gmail.com>
2021-10-14 13:31:34 -0400

    movie description completed

--- script.js
+++ script.js
@@ -5,15 +5,35 @@ fetch(url)
         return res.json();
     }).then((movies)=>{
         console.log(movies);
-        for(let movie of movies){
-            console.log(movie.title)
+        /////// categories ///////
         let select = document.querySelector("#select-movie");
-        ///create options
-        let newOption = document.createElement("option");
-        newOption.textContent = movie.title;
-        newOption.value = movie.title
-        select.append(newOption)
+
+        for(let movie of movies){
+            let newOption = document.createElement("option");
+            newOption.textContent = movie.title;
+            newOption.value = movie.title;
+            select.append(newOption);
+            /////// ///////
         }
+        let names = document.querySelectorAll("#select-movie")
+        /////Movie Description //// 
+
+
+        select.addEventListener("change",(e)=>{
+            console.log(e.target.value);
+            let container = document.querySelector("#display-info-container");
+            let movieTitle = document.createElement("h3");
+            let year = document.createElement("p");
+            let description = document.createElement("p")
+
+            for(let movie of movies){
+            movieTitle.textContent = e.target.value;
+            year.textContent = movie.release_date;
+            description.textContent = movie.description;
+           }
+        container.append(movieTitle, year, description)
+        })
+        ///////////
     }).catch((err)=>{
         console.log(err)
     })
